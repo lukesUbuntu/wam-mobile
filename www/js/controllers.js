99421667c1e8a379b0944dc0a3cc0b60aa8f555f
Luke Hardiman <luke@hardiman.co.nz>
2015-08-01 23:55:20 +1200

    Added news
    Added news, loading screen fixed up some naming standards

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,35 +1,71 @@
+//need to replace with phone locations
+var prams = {
+    //getgps data
+    //longitude=174.777290&latitude=-41.306481
+    longitude : 174.777290,
+    latitude : -41.306481
+}
+
 var app = angular.module('starter.controllers', [])
 
 app.controller('DashCtrl', function($scope) {})
 
 //News Controller
-app.controller('NewsCtrl', function($scope) {})
+app.controller('NewsCtrl', function($scope,$ionicLoading,wamApi) {
 
-//Chats Controller
-app.controller('EventCtrl', function($scope, wamApi) {
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
-        var prams = {
-            //getgps data
-            //longitude=174.777290&latitude=-41.306481
-            longitude : 174.777290,
-            latitude : -41.306481
+    $scope.show = function () {
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+    };
+    $scope.hide = function () {
+        $ionicLoading.hide();
+    };
+
+    $scope.show();
+    //get the events
+    wamApi('getNews', prams).then(function (response) {
+        //console.log("getNews", response)
+        if (response.success) {
+            var $data = JSON.parse(response.data);
+            console.log("$data",$data)
+            $scope.news_items = $data;
+            $scope.hide();
         }
 
-        wamApi('getEvents', prams).then(function (response) {
-            console.log("getEvents", response)
-            if (response.success) {
-                var $data = JSON.parse(response.data);
-                console.log("$data",$data.events)
-                $scope.events = $data.events;
-            }
+    })
+})
 
-        })
+//Chats Controller
+app.controller('EventCtrl', function($scope, $ionicLoading,wamApi) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    $scope.show = function () {
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+    };
+    $scope.hide = function () {
+        $ionicLoading.hide();
+    };
+
+    $scope.show();
+    //get the events
+    wamApi('getEvents', prams).then(function (response) {
+        //console.log("getEvents", response)
+        if (response.success) {
+            var $data = JSON.parse(response.data);
+            console.log("$data",$data.events)
+            $scope.event_items = $data.events;
+            $scope.hide();
+        }
+
+    })
 });
 
 
@@ -50,9 +86,4 @@ app.controller('RoadCtrl', function($scope) {
 //Weather
 app.controller('WeatherCtrl', function($scope) {
 
-})
-
-
-
-
-
+});
